7599102995bfc52c9b11c702c454697b5f2c29ac
David Mclean <47851457+davupls@users.noreply.github.com>
2023-04-27 18:11:04 -0500

    Initial Commit

---
+++ FrostyScoopv2/FrostyScoopv2App.swift
@@ -0,0 +1,17 @@
+//
+//  FrostyScoopv2App.swift
+//  FrostyScoopv2
+//
+//  Created by David Mclean on 4/27/23.
+//
+
+import SwiftUI
+
+@main
+struct FrostyScoopv2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
