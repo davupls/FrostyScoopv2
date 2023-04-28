6514fbe143a899d944c66b1f3b83e57b667df69d
David Mclean <47851457+davupls@users.noreply.github.com>
2023-04-28 02:49:35 -0500

    -- generic safe UserCartView commit

--- FrostyScoopv2/UserCartView.swift
+++ FrostyScoopv2/UserCartView.swift
@@ -12,14 +12,66 @@ struct UserCartView: View {
     
     var body: some View {
         VStack(alignment: .leading) {
+            header
+            itemsList
+            
+        }
+        .padding()
+    }
+    
+    
+    
+    
+    var header: some View {
+        HStack {
             Text("Shopping Cart: ")
                 .font(.title)
-            ForEach(cartModel.EachItem!) { item in
-                Text(item.itemName)
-            }
-            Spacer()
+                .padding()
         }
     }
+    
+    var itemsList: some View {
+        ScrollView {
+            ForEach(0..<5) { item in
+                HStack {
+                    Image("chocolate gelato")
+                            .resizable()
+                            .scaledToFit()
+                            .frame(width: 60, height: 60)
+                        
+                        VStack {
+                            Text("Chocolate Gelato")
+                            
+                            Text("\(4.56, specifier: "%.2f")")
+                        }
+                        .padding()
+                        
+                        
+                        HStack {
+                            ZStack {
+                                Circle()
+                                    .frame(width: 30)
+                                    .foregroundColor(.white)
+                                    .shadow(radius: 4)
+                                Image(systemName: "minus")
+                            }
+                            Text("1")
+                            ZStack {
+                                Circle()
+                                    .frame(width: 30)
+                                    .foregroundColor(.white)
+                                    .shadow(radius: 4)
+                                Image(systemName: "plus")
+                            }
+                            
+                            Image(systemName: "trash")
+                        }
+                }
+            }
+        }
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+    
 }
 
 struct UserCartView_Previews: PreviewProvider {
@@ -28,3 +80,48 @@ struct UserCartView_Previews: PreviewProvider {
             .environmentObject(CartItemViewModel())
     }
 }
+
+
+
+
+//var itemsList: some View {
+//    ScrollView {
+//        ForEach(cartModel.EachItem!) { item in
+//            HStack {
+//                Image(item.itemImage)
+//                        .resizable()
+//                        .scaledToFit()
+//                        .frame(width: 60, height: 60)
+//
+//                    VStack {
+//                        Text(item.itemName)
+//
+//                        Text("\(item.itemcost, specifier: "%.2f")")
+//                    }
+//                    .padding()
+//
+//
+//                    HStack {
+//                        ZStack {
+//                            Circle()
+//                                .frame(width: 30)
+//                                .foregroundColor(.white)
+//                                .shadow(radius: 4)
+//                            Image(systemName: "minus")
+//                        }
+//                        Text("1")
+//                        ZStack {
+//                            Circle()
+//                                .frame(width: 30)
+//                                .foregroundColor(.white)
+//                                .shadow(radius: 4)
+//                            Image(systemName: "plus")
+//                        }
+//
+//                        Image(systemName: "trash")
+//                    }
+//            }
+//        }
+//    }
+//    .frame(maxWidth: .infinity, alignment: .leading)
+//}
