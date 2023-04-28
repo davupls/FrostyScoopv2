feae46a94f3759fb305ba51a16d680b747884ce4
David Mclean <47851457+davupls@users.noreply.github.com>
2023-04-27 22:47:21 -0500

    - filter decodedIceCreamJson with map method to separator flavors into new variable for easier access - new published variable FalvorsData - new state variable typeTracker to keep count of which Flavor array position to display in Flavors Card View

--- FrostyScoopv2/MainView.swift
+++ FrostyScoopv2/MainView.swift
@@ -9,23 +9,29 @@ import SwiftUI
 
 struct MainView: View {
     @ObservedObject var IceCreamModel = IceCreamsViewModel()
+    @State private var typeTracker = 0
+    
     
     var body: some View {
         VStack {
             homeHeader
-            ScrollView(.horizontal){
-                HStack{
-                    ForEach(IceCreamModel.decodedIceCreamJson) { CreamType in
-                        VStack {
-                            Text(CreamType.name)
-                        }.padding()
+            icecreamTypes
+            
+            VStack {
+                
+                ScrollView(.horizontal) {
+                    HStack {
+                        ForEach(Array(IceCreamModel.FlavorsData[typeTracker])) { flavors in
+                            Text(flavors.name)
+                                .padding()
+                        }
                     }
                 }
+                
             }
+            .padding()
         }
-        .padding()
     }
-    
     
     
     
@@ -42,7 +48,30 @@ struct MainView: View {
             Spacer()
             Image(systemName: "globe")
                 .imageScale(.large)
-            .foregroundColor(.accentColor)
+                .foregroundColor(.accentColor)
+        }
+    }
+    
+    // MARK: IceCream Types Horizontal View
+    var icecreamTypes: some View {
+        ScrollView(.horizontal){
+            HStack{
+                ForEach(IceCreamModel.decodedIceCreamJson) { CreamType in
+                    Button {
+                        typeTracker = CreamType.id - 1
+                    } label: {
+                        VStack {
+                            Image(CreamType.image)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 100, height: 100)
+                            Text(CreamType.name)
+                                .foregroundColor(.black)
+                        }.padding()
+                    }
+                    
+                }
+            }
         }
     }
     
